6458d42492cb5c8047d48a53cb95a7d162363277
Aye Thuzar <ayethuzar@gmail.com>
2023-07-24 03:26:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ from datasets import load_dataset
 
 st.title('Can I Patent This?')
 
+# loading the dataset
+dataset_dict = load_dataset('HUPD/hupd',
+    name='sample',
+    data_files="https://huggingface.co/datasets/HUPD/hupd/blob/main/hupd_metadata_2022-02-22.feather",
+    icpr_label=None,
+    train_filing_start_date='2016-01-01',
+    train_filing_end_date='2016-01-21',
+    val_filing_start_date='2016-01-22',
+    val_filing_end_date='2016-01-31',
+)
+
 # steamlit form
 option = st.selectbox(
     'How would you like to be contacted?',
