518b2cf27bbc1021e7000c30f528cf66add744f1
Aye Thuzar <ayethuzar@gmail.com>
2023-07-24 10:12:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,25 @@ st.title('Can I Patent This?')
 
 st.write("This model is tuned with all patent applications submitted in Jan 2016 in [the Harvard USPTO patent dataset](https://github.com/suzgunmirac/hupd)")
 
+# to upload a .csv file
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+
 tuple_of_choices = ('patent_number', 'title', 'background', 'summary', 'description')
 
 # steamlit form
