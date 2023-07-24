d9a4f25d279af51e1a78d8c92a8e9f138ddba50e
Aye Thuzar <ayethuzar@gmail.com>
2023-07-24 10:57:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ model_name = "ayethuzar/tuned-for-patentability"
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-test = [user_input_abstract[0] + user_input_claims[0]
+test = [user_input_abstract[0] + user_input_claims[0]]
 
 if submit:
     batch = tokenizer(test, padding = True, truncation = True, max_length = 512, return_tensors = "pt")
