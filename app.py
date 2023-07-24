9de6f34922d5dc2de70ecd395b7d8e614774d3a7
Aye Thuzar <ayethuzar@gmail.com>
2023-07-24 03:15:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 st.title('Can I Patent This?')
 
 # steamlit form
+option = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone'))
+
+st.write(option)
+
 form = st.form(key='sentiment-form')
 user_input = form.text_area(label = 'Enter your text', value = "I love steamlit and hugging face!")
 submit = form.form_submit_button('Submit')
