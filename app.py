2125690c99fea417960276b4ab6158940b4aaf18
Aye Thuzar <ayethuzar@gmail.com>
2023-07-24 03:07:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ if submit:
       outputs = model(**batch)
       #st.write(outputs)
       predictions = F.softmax(outputs.logits, dim = 1)
-      st.write(predictions)
-      labels = torch.argmax(predictions, dim = 1)
-      st.write(labels)
+      result = "Patentability Score: " + str(predictions.numpy()[1])
+      html_str = f"""<style>p.a {{font: bold {28}px Courier;color:#1D5D9B;}}</style><p class="a">{result}</p>"""
+      st.markdown(html_str, unsafe_allow_html=True)
+      
